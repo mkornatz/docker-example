437e76aaebb02308587e9b5becd77f01ed83db46
mkornatz <mkornatz@gmail.com>
2017-01-28 20:43:56 -0500

    Fixes ownership of /var/www/html

--- public/index.php
+++ public/index.php
@@ -1,5 +1,15 @@
 <?php
 
+  // Test writing a file
+  $filename = 'test.txt';
+  $fp = fopen($filename, 'w');
+  if (fwrite($fp, 'Hello file.') === FALSE) {
+    echo "Cannot write to test file ($filename)";
+    exit;
+  }
+  fclose($fp);
+  unlink($filename);
+
   print_r('<div style="text-align:center">You got docker set up! Here are the details on PHP:</div>');
 
   phpinfo();
